4a06f29ad563c89ca51f8bf38e4f5eafe0651142
Jean Urena <juanpedro1519@gmail.com>
2021-04-16 23:39:33 -0400

    Adding proptypes library

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 
 // first react component
 const App = ({title}) => {
   return <div>{title}</div>;
+};
+
+App.propTypes = {
+  title: Proptypes.string
 };
 
 export default App;
